7c458d5a6ab0a2a187e4eefa4309d4c3d25fe8ff
Smalhotra1996 <sonam.malhotra2496@gmail.com>
2023-03-03 10:36:52 -0500

    Created a blog-website using Node.js, Express, HTML, CSS and javaScript

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -17,9 +18,43 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+let posts = [];
 
 app.get("/", function (req, res) {
-  res.render("home", { startingContent: homeStartingContent });
+  res.render("home", { startingContent: homeStartingContent, posts: posts });
+});
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+app.get("/compose", function (req, res) {
+  res.render("compose", {});
+});
+app.post("/compose", function (req, res) {
+  console.log(req.body.postTitle);
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+
+  posts.push(post);
+  res.redirect("/");
+});
+
+app.get("/posts/:postName", function (req, res) {
+  const reuestedTitle = _.lowerCase(req.params.postName);
+  posts.forEach(function (post) {
+    const storedTitle = _.lowerCase(post.title);
+    if (storedTitle === reuestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+      });
+    }
+  });
 });
 
 app.listen(3100, function () {
